fead0aac743d64ef12aedb070effdc2b73a6b44e
ayeon598 <165542857+ayeon598@users.noreply.github.com>
2025-02-19 21:38:55 +0900

    02-18

--- practice/sw_4869.py
+++ practice/sw_4869.py
@@ -1,6 +1,13 @@
+def dp(n):
+    dp = [0] * (n+1)
+    dp[10] = 1
+    dp[20] = 3
+    
+    for i in range(30, n+1, 10):
+        dp[i] = dp[i-10] +2 * dp[i-20]
+    return dp[n]
+
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
-    if N == 10: print(f'#{t} 1')
-    else:
-        for i in range(1, N//10):
+    print(f'#{t} {dp(N)}')
