ec6b1638505802bf2ef121ac81f5f91e1c91ae04
ayeon598 <165542857+ayeon598@users.noreply.github.com>
2025-03-05 23:33:47 +0900

    03-05

--- algo/sw_5174.py
+++ algo/sw_5174.py
@@ -2,7 +2,15 @@ T = int(input())
 for t in range(1, T+1):
     E, N = map(int, input().split())
     arr = list(map(int, input().split()))
-    left = [0] * (E+2)
-    right = [0] * (E+2)
-    par = [0] * (E+2)
-
+    node = [[0] * (E+2) for _ in range(E+2)]
+    lst = [N]
+    k = 0
+    for i in range(E):
+        node[arr[2*i]][arr[2*i+1]] = 1
+    while True:
+        for i in range(E+2):
+            if node[lst[k]][i] == 1:
+                lst.append(i)
+        k += 1
+        if k >= len(lst): break
+    print(f'#{t} {len(lst)}')
