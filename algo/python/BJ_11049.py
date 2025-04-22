a82123d2c735d9c01b71f3c1f41b4ff201b13889
ayeon598 <165542857+ayeon598@users.noreply.github.com>
2025-04-22 23:00:13 +0900

    04-22

--- algo/python/BJ_11049.py
+++ algo/python/BJ_11049.py
@@ -1,5 +1,14 @@
+def dp(arr, n):
+    dat = [[0] * n for _ in range(n)]
+    for i in range(1, N):
+        for j in range(N):
+            if i + j == N: break
+            dat[j][i+j] = float('inf')
+            for k in range(j, j+i):
+                dat[j][j+i] = min(dat[j][j+i], dat[j][k] + dat[k+1][j+i] + arr[j][0]*arr[k][1]*arr[j+i][1])
+    return dat[0][n - 1]
+
 N = int(input())
-arr = []
-for _ in range(N):
-    r, c = map(int, input().split())
-    arr.append((r, c))
+arr = [tuple(map(int, input().split())) for _ in range(N)]
+result = dp(arr, N)
+print(result)
