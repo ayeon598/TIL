555d4ee18a06d2aa35c3eb94459f023ce74dabcc
ayeon598 <165542857+ayeon598@users.noreply.github.com>
2025-04-28 00:49:16 +0900

    04-28

---
+++ algo/BAEKJOON/src/Bronz11382.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Bronz11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        System.out.println(a+b+c);
+    }
+}
