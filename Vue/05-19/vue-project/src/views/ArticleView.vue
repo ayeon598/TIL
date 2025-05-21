708f791f8c928e7544635c0ee77cae71208cafe3
염아연 <dus1094@naver.com>
2025-05-22 08:43:12 +0900

    05-21

---
+++ Vue/05-19/vue-project/src/views/ArticleView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Articles</h1>
+    <RouterLink :to="{ name:'CreateView'}">
+      [CREATE]
+    </RouterLink>
+    <!-- 전체 게시글 목록 -->
+    <ArticleList />
+  </div>
+</template>
+
+<script setup>
+  import ArticleList from '@/components/ArticleList.vue'
+  import { onMounted } from 'vue'
+  import { useArticleStore } from '@/stores/articles'
+  import { RouterLink } from 'vue-router'
+
+  const store = useArticleStore()
+
+  onMounted(() => {
+    store.getArticles()
+  })
+
+
+</script>
+
+<style>
+
+</style>
